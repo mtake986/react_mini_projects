0410d7cca1478953fdc1450223595c62b615bb69
Masahiro Takechi <tak21011@byui.edu>
2022-02-23 16:31:44 -0700

    add todos, delete todos, filtering, store in a localStorage

--- ed_todo/src/App.js
+++ ed_todo/src/App.js
@@ -1,11 +1,73 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
+// importing components
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  
+  // todo: get local todos
+  useEffect(() => {
+    getLocalTodos()
+  }, [])
+
+  useEffect(() => {
+    handleFilter();
+    saveLocalTodos();
+  }, [todos, status]);
+
+  const handleFilter = () => {
+    switch (status) {
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
+  // Save to Local
+  const saveLocalTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  };
+  //
+  const getLocalTodos = () => {
+    if (localStorage.getItem('todos') === null) {
+      localStorage.setItem('todos', JSON.stringify([]));
+    } else {
+      const localData = JSON.parse(localStorage.getItem('todos', JSON.stringify(todos)));
+      setTodos(localData)
+    }
+  };
   return (
-    <div className="App">
-      <h1>Hello react!</h1>
-    </div>
+    <>
+      <header>
+        <h1>Masa's Todo List</h1>
+      </header>
+      <Form
+        inputText={inputText}
+        setInputText={setInputText}
+        todos={todos}
+        setTodos={setTodos}
+        setStatus={setStatus}
+        status={status}
+      />
+      <TodoList
+        setTodos={setTodos}
+        todos={todos}
+        filteredTodos={filteredTodos}
+      />
+      ;
+    </>
   );
 }
 
