8ac2e455d23669305bcf007e740682e5e0d5765d
Masahiro Takechi <tak21011@byui.edu>
2022-03-01 13:43:00 -0700

    enable to edit,

--- wds_markdown_blog_MErN/models/article.js
+++ wds_markdown_blog_MErN/models/article.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
-const marked = require('marked')
+const marked = require("marked")
 const slugify = require('slugify')
 const createDomPurify = require('dompurify')
-const {JSDOM} = require('jsdom')
+const { JSDOM } = require('jsdom')
 const dompurify = createDomPurify(new JSDOM().window)
+
+
 const articleSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -24,6 +26,10 @@ const articleSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true
+  },
+  sanitizedHtml: {
+    type: String,
+    required: true
   }
 })
 
@@ -31,6 +37,9 @@ articleSchema.pre('validate', function (next) {
   if (this.title) {
     this.slug = slugify(this.title, { lower: true, strict: true })
   }
+  // if (this.markdown) {
+  //   this.sanitizedHtml = dompurify.sanitize(marked(this.markdown))
+  // }
   next()
 })
 
--- wds_markdown_blog_MErN/routes/articles.js
+++ wds_markdown_blog_MErN/routes/articles.js
@@ -3,49 +3,62 @@ const Article = require('./../models/article')
 const router = express.Router()
 
 router.get('/new', (req, res) => {
+  console.log('new')
   res.render('articles/new', { article: new Article() })
+  console.log('new done')
+})
+
+router.get('/edit/:id', async (req, res) => {
+  const article = await Article.findById(req.params.id)
+  console.log(article)
+  res.render('articles/edit', { article: article })
 })
 
 router.get('/:slug', async (req, res) => {
-  console.log("obfnsav")
-  try {
-    console.log("try")
-    const article = await Article.findOne({ slug: req.params.slug })
-    if (article == null) res.redirect('/')
-    console.log(article)
-    res.render('articles/show', { article: article })
-  } catch (e) {
-    console.log("catch")
-    console.log(e)
-  }
-  // res.render('articles/show', { article: article })
+  console.log("try")
+  const article = await Article.findOne({ slug: req.params.slug })
+  if (article == null) res.redirect('/')
+  // console.log(article)
+  res.render('articles/show', { article: article })
 })
 
-router.post('/', async (req, res) => {
-  console.log('1')
-  let article = new Article({
-    title: req.body.title,
-    description: req.body.description,
-    markdown: req.body.markdown,
-  })
-  try {
-    console.log('2')
-    article = await article.save()
-    console.log('2.1')
-    res.redirect(`/articles/${article.slug}`)
-    console.log('2.2')
-  } catch (e) {
-    console.log('3')
-    console.log(e)
-    res.render('articles/new', { article: article })
-  }
-})
+router.post('/', async (req, res, next) => {
+  req.article = new Article()
+  next()
+}, saveArticleAndRedirect('new'))
 
-method="DELETE"
+router.put('/:id', async (req, res, next) => {
+  req.article = await Article.findById(req.params.id)
+  next()
+}, saveArticleAndRedirect('edit'))
+
+// method="DELETE"
 router.delete('/:id', async (req, res) => {
   await Article.findByIdAndDelete(req.params.id)
   res.redirect('/')
 })
+
+function saveArticleAndRedirect(path) {
+  return async (req, res) => {
+    let article = req.article
+    article.title = req.body.title
+    article.description = req.body.description
+    article.markdown = req.body.markdown
+    article.sanitizedHtml = req.body.markdown
+    try {
+      console.log('saveArticleAndRedirect: 2')
+      article = await article.save()
+      console.log('saveArticleAndRedirect: 2.1')
+      res.redirect(`/articles/${article.slug}`)
+      console.log('saveArticleAndRedirect: 2.2')
+    } catch (e) {
+      console.log('saveArticleAndRedirect: 3.1')
+      console.log(e) 
+      res.render(`articles/${path}`, { article: article })
+    }
+  }
+}
+
 
 module.exports = router
 
@@ -56,3 +69,6 @@ router.get('[LINK]', (req, res) => {
 */
 
 // ? await article.save() => this is asynchronous function so we just wanna make sure we set this up inside of our own async function and we await this article that save 
+
+// ? res.redirect(someURL) is for you want to return a 30x status code (often 302) to the browser and tell the browser to go to a new URL. This is often done on the server as part of a login process and occasionally when the user finds themselves at an old URL that has been changed to something different. res.redirect() should only be used for these types of forced navigation to a different URL and should never be part of any standard rendering process.
+// ? res.render(filename, data) is how one would typically use EJS (or any other template engine plugged into Express) to fill in a template with some data and return the "rendered" HTML page back to the requesting browser so the browser can render it.
